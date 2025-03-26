9c8029f42265ba56ca13c5acf8d0302464696848
Manish Singh <manishsingh704293@gmail.com>
2025-03-27 00:56:19 +0530

    Create 01_Set_Matrix_Zero.cpp

---
+++ Striver/01_Set_Matrix_Zero.cpp
@@ -0,0 +1,30 @@
+    void setZeroes(vector<vector<int>>& matrix) {
+      int n = matrix.size();
+      int m = matrix[0].size();
+
+        vector<int>x,y;
+
+        for(int i =0; i<n; i++){
+            for (int j = 0; j<m; j++){
+                if(matrix[i][j] == 0){
+                    x.push_back(i);
+                    y.push_back(j);
+                }
+            }
+        }
+
+        for(int i =0; i<x.size(); i++){
+            for (int j = 0; j<m; j++){
+                matrix[x[i]][j] = 0;
+            }
+        }
+        for(int i =0; i<n; i++){
+            for (int j = 0; j<y.size(); j++){
+              matrix[i][y[j]] = 0;
+            }
+        }
+
+    }
+
+// Time Complexity :- O(N*M)
+// Space Complexity :- O(N+M)
