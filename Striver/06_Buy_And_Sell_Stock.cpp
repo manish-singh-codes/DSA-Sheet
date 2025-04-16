cdca7223287bb4f419c70a9d49467c4a675985e9
Manish Singh <manishsingh704293@gmail.com>
2025-04-17 00:36:50 +0530

    Create 06_Buy_And_Sell_Stock.cpp

---
+++ Striver/06_Buy_And_Sell_Stock.cpp
@@ -0,0 +1,13 @@
+    int maxProfit(vector<int>& prices) {
+        int maxProfit = 0, profit = 0;
+        int mini = INT_MAX;
+        for(int i =0; i<prices.size(); i++){
+            mini = min(mini,prices[i]);
+            profit = prices[i] - mini;
+            maxProfit = max(maxProfit,profit);
+        }
+        return maxProfit;
+    }
+
+// Time Complexity :- O(N)
+// Space Complexity :- O(1)
