6222bf453e16be9a8cd033d2e1ca01a8a61eb215
Manish Singh <manishsingh704293@gmail.com>
2025-06-05 00:54:28 +0530

    Create 04_Merge_Sort.cpp

---
+++ Striver/Sorting/04_Merge_Sort.cpp
@@ -0,0 +1,45 @@
+    void merge(vector<int>&arr, int l, int mid, int r){
+        int left = l;
+        int right = mid+1;
+        vector<int>temp;
+        
+        while(left <= mid && right <= r){
+            if(arr[left] <= arr[right]){
+                temp.push_back(arr[left]);
+                left++;
+            }
+            else{
+                temp.push_back(arr[right]);
+                right++;
+            }
+        }
+        
+        while(left<=mid){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        while(right<=r){
+            temp.push_back(arr[right]);
+            right++;
+        }
+        
+          for(int i=l; i<=r; i++){
+            
+            arr[i] = temp[i-l];
+            
+        }
+    }
+  
+    void mergeSort(vector<int>& arr, int l, int r) {
+        // code here
+        if(l == r) return;
+        
+        int mid = l + (r-l)/2;
+        mergeSort(arr,l,mid);
+        mergeSort(arr,mid+1,r);
+        merge(arr,l, mid, r);
+        
+    }
+
+// Time Complexity :- O(N long N)
+// Space Complexity :- O(N)
