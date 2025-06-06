538e1f60c27db92aaa7d0136d04691794ed21993
Manish Singh <manishsingh704293@gmail.com>
2025-06-06 23:20:45 +0530

    Update 01_Set_Matrix_Zero.cpp

--- Striver/01_Set_Matrix_Zero.cpp
+++ Striver/01_Set_Matrix_Zero.cpp
@@ -1,7 +1,7 @@
     void setZeroes(vector<vector<int>>& matrix) {
       int n = matrix.size();
       int m = matrix[0].size();
-        vector<int>x,y;
+      vector<int>x,y;
 
         for(int i =0; i<n; i++){
             for (int j = 0; j<m; j++){
