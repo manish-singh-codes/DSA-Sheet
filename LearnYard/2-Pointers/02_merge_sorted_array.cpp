d1aa37a10de838fbe00ac0cbbf6b6a991444acf8
Manish Singh <manishsingh704293@gmail.com>
2025-06-17 23:06:22 +0530

    Update 02_merge_sorted_array.cpp

--- LearnYard/2-Pointers/02_merge_sorted_array.cpp
+++ LearnYard/2-Pointers/02_merge_sorted_array.cpp
@@ -1,4 +1,5 @@
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int i = m-1;
         int j = n-1;
         int k = (n+m)-1;
