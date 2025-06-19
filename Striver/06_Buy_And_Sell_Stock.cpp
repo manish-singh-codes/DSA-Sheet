f4c907f5f26fd8bbf6b780f387ade5625ab64ef5
Manish Singh <manishsingh704293@gmail.com>
2025-06-20 00:32:20 +0530

    Update 06_Buy_And_Sell_Stock.cpp

--- Striver/06_Buy_And_Sell_Stock.cpp
+++ Striver/06_Buy_And_Sell_Stock.cpp
@@ -1,4 +1,4 @@
-    int maxProfit(vector<int>& prices) {
+int maxProfit(vector<int>& prices) {
         int maxProfit = 0, profit = 0;
         int mini = INT_MAX;
         for(int i =0; i<prices.size(); i++){
